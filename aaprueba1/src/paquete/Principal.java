e728a8f4cdd6b29a143b299add4c1f2b92f58152
KerriCuura <dam_miguelmanzanilla@riberadeltajo.es>
2023-02-14 16:04:15 +0100

    cosas feas en el syso

--- aaprueba1/src/paquete/Principal.java
+++ aaprueba1/src/paquete/Principal.java
@@ -3,6 +3,6 @@ package paquete;
 public class Principal {
 public static void main(String[] args) {
 	System.out.println("Hola mundo");
-	System.out.println("el cambio de la clase principal");
+	System.out.println("el cambio de la clase principal fdehufhewiotfheworfgeriuthwer9toerfg");
 }
 }
