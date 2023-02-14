f01763c5d909dcddb0a5117a5b531a0c7213c712
KerriCuura <dam_miguelmanzanilla@riberadeltajo.es>
2023-02-14 16:00:05 +0100

    enviado

--- aaprueba1/src/paquete/Principal.java
+++ aaprueba1/src/paquete/Principal.java
@@ -3,5 +3,6 @@ package paquete;
 public class Principal {
 public static void main(String[] args) {
 	System.out.println("Hola mundo");
+	System.out.println("el cambio de la clase principal");
 }
 }
